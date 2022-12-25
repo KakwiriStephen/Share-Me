aab8932017f9ea82d0db17451cdce195540bd340
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-25 15:53:31 +0300

    PinSchema

--- backend/shareme/schemas/pin.js
+++ backend/shareme/schemas/pin.js
@@ -3,4 +3,21 @@ export default {
   name: 'pin',
   title: 'Pin',
   type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      title: 'About',
+      type: 'string',
+    },
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+  ],
 }
