d32b49500196c08181de9a442b4aef29027983ce
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-26 15:22:52 +0300

    Importing Schemas

--- backend/shareme/schemas/index.js
+++ backend/shareme/schemas/index.js
@@ -3,3 +3,7 @@
 export const schemaTypes = [user]
 
 import user from './user'
+import pin from './pin'
+import comment from './comment'
+import postedBy from './postedBy'
+import save from './save'
