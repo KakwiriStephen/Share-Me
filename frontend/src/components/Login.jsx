ddc7d3bb915e3f1aab96ba8d81eda876ae725870
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-27 16:29:11 +0300

    Login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 function login() {
   return <div>login</div>;
