f512b0c3a4aaa581692e1df4526bc4222b052152
KakwiriStephen <ngottakakwiri@gmail.com>
2022-12-30 22:11:11 +0300

    Project ID

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,7 +2,7 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: 2021 - 11 - 16,
   useCdn: true,
