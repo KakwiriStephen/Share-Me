e5a2e02a53998143e33e124ec34c41b6eac6fd73
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-01 16:52:26 +0300

    Debugging

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import createRoot from "react-dom";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
+createRoot(
   <Router>
     <App />
   </Router>,
   document.getElementById("root")
 );
+import { createRoot } from "react-dom";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello World!</h1>
+//     </div>
+//   );
+// }
+
+// const root = document.getElementById('root');
+// createRoot(root).render(<App />);
