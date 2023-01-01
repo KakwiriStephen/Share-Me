ecba24b6961004b20e91b070f7ae55d12f4aea7c
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-01 17:32:01 +0300

    Debugging

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { gapi } from "gapi-script";
+import { useEffect } from "react";
 import GoogleLogin from "react-google-login";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
