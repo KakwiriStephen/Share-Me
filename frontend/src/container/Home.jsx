1951288db2e5e878182a21da393f57fe204643d4
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-05 09:32:14 +0300

    Home Page

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { Link, Route, Routes } from "react-router-dom";
 
-function Home() {
+const Home = () => {
   return <div>Home</div>;
-}
+};
 
 export default Home;
