133e6ec34ec9141584b9a3747480005497ecc481
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-06 15:06:54 +0300

    SideBar

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
             <img src={logo} alt="logo" className="w-28" />
           </Link>
           <Link to={`user-profile/${user?._id}`}>
-            <img src={user?.image} alt="l" className="w-28" />
+            <img src={user?.image} alt="l" className="w-10 rounded-full" />
           </Link>
         </div>
         {toggleSidebar && (
