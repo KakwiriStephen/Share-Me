4fc253035b45bd59cbf1d513259b5fb4e9bf0bc3
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-06 11:01:13 +0300

    Sidebar Logo

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -6,7 +6,15 @@ import { IoIosArrowFoward } from "react-icons/io";
 import logo from "../assets/logo.png";
 
 const Sidebar = ({ user, closeToggle }) => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
+      <div className="flex flex-col">
+        <Link to="/" className=" flex px-5 gap-2 my-6 pt-1 w-190 items-center">
+          <img src={logo} alt="logo" className="w-full" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
