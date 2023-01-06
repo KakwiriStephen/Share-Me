212f6e55ef24d2d70dc835286420795ee4b57b34
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-06 15:19:03 +0300

    Pin

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components";
 
 const Pins = () => {
   return <div>Pins</div>;
