ece58785ac55b15c9f5e0065b60fd03297552fcf
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-07 15:32:35 +0300

    NavbarProps

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -6,7 +6,13 @@ import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components";
 
 const Pins = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  return <div>Pins</div>;
+  return (
+    <div className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </div>
+  );
 };
 
 export default Pins;
