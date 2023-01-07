9539fe10222b0c5a24b6e3e260729d4867348cba
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-07 15:27:31 +0300

    SearcTerm

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import { Navbar, Feed, PinDetail, CreatePin, Search } from "../components";
 
 const Pins = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return <div>Pins</div>;
 };
 
