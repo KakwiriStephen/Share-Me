7fea72e847c03f57b38951f0e6a4ea6e2f5a4533
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-07 17:36:42 +0300

    RingLoader

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import Loader from "react-loader-spinner";
+// import Loader from "react-loader-spinner";
+import { Rings } from "react-loader-spinner";
+// import * as Loader from "react-loader-spinner";
 
 const Spinner = ({ message }) => {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-      <Loader
+      <Rings
         type="Cirles"
         color="#00BFFF"
         height={50}
         width={200}
         className="m-5"
       />
+      <p className="text-lg text-center px-2">{message}</p>
     </div>
   );
 };
