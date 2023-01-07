44c48aedaed885e4f295466de2f29b915b2b84fd
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-07 17:27:28 +0300

    Spinner

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import Loader from "react-loader-spinner";
 
 const Spinner = ({ message }) => {
-  return <div>Spinner</div>;
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <Loader
+        type="Cirles"
+        color="#00BFFF"
+        height={50}
+        width={200}
+        className="m-5"
+      />
+    </div>
+  );
 };
 
 export default Spinner;
