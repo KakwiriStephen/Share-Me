83d627f64f09503a1eb16d5ebd49ab8e824a1be1
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-07 15:49:01 +0300

    Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { IoMdAdd, IoMdSearch } from "react-icons/oi";
 
 const Navbar = () => {
   return <div>Navbar</div>;
