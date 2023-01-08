20972ef0306cce24781e7e0e5f52b6eca529822e
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-08 13:02:17 +0300

    Bug

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -7,7 +7,7 @@ import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
 
 const Feed = () => {
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
 
   if (loading)
     return <Spinner message="We are adding new ideas to your Feed" />;
