35460d2a17fe7fdd8cfe78428a26de0c0316b192
KakwiriStephen <ngottakakwiri@gmail.com>
2023-01-08 14:24:16 +0300

    Category

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -7,7 +7,16 @@ import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
 
 const Feed = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams;
+
+  useEffect(() => {
+    setLoading(true);
+
+    if (categoryId) {
+    } else {
+    }
+  }, [categoryId]);
 
   if (loading)
     return <Spinner message="We are adding new ideas to your Feed" />;
